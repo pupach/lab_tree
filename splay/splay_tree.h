3948ec5b0ad435f9e5ae0d644515f968172ceb55
pupach <58131107+pupach@users.noreply.github.com>
2024-05-21 23:03:48 +0300

    Update splay_tree.h

--- splay/splay_tree.h
+++ splay/splay_tree.h
@@ -20,9 +20,22 @@
   (a > b ? a : b)
 #define MAX_SIZE_COM 3
 
-
+/**
+ * @brief Вставляет узел в дерево
+ * 
+ * @param[in] указатель на узел, являющийся корнем дерева
+ * @param[in] Узел для вставки
+ *  @return новый узел, являющийся корнем дерева
+ */
 node *InsertSplay(node *tree, node *ElemToIns);
 
+/**
+ * @brief Удаляет имеющийся ключ из дерева
+ * 
+ * @param[in] указатель на узел, являющийся корнем дерева
+ * @param[in] значение, подлежащее удалению
+ *  @return новый узел, являющийся корнем дерева
+ */
 node *RemoveSPlay(node *head, int elem);
 
 #endif //LAB_TREE_SPLAY_TREE_H
