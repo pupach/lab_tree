9a273d9fecedd8f2fa68b853e1cf57d0147ed4d0
pupach <58131107+pupach@users.noreply.github.com>
2024-05-21 23:11:20 +0300

    Update rb_tree.h

--- rb/rb_tree.h
+++ rb/rb_tree.h
@@ -28,8 +28,22 @@ typedef struct
     struct ElemRBT* left;
 }ElemRBT;
 
+/**
+ * @brief Вставляет узел в дерево
+ * 
+ * @param[in] указатель на узел, являющийся корнем дерева
+ * @param[in] Узел для вставки
+ *  @return новый узел, являющийся корнем дерева
+ */
 ElemRBT *InsertRBT(ElemRBT *head, ElemRBT *elem);
 
+/**
+ * @brief Удаляет имеющийся ключ из дерева
+ * 
+ * @param[in] указатель на узел, являющийся корнем дерева
+ * @param[in] значение, подлежащее удалению
+ *  @return новый узел, являющийся корнем дерева
+ */
 ElemRBT *RemoveRBT(ElemRBT *head, int val);
 
 #endif //LAB_TREE_REDBLACKTREE_H
